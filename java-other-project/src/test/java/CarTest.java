66fd4b7b941a3427923cd10d2d8f34625bf57ca4
prashanth.gns <prashanth.gns@collabera.com>
2022-01-04 16:49:35 +0530

    java recap : test car

---
+++ java-other-project/src/test/java/CarTest.java
@@ -0,0 +1,39 @@
+import clsobj.Car;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @DisplayName("car invalid object")
+    @Test
+    void testCarInvalidObject() {
+        Car car = new Car();
+        car = null;
+        Assertions.assertNull(car);
+    }
+    @DisplayName("cae valid object")
+    @Test
+    void testCarValidObject()
+    {
+        Car car = new Car();
+        Assertions.assertNotNull(car);
+    }
+    @DisplayName("car Speed by Default Constructor")
+    @Test
+    void testCarSpeedDefault(){
+        Car car = new Car();
+        int expectedSpeed = 10;
+        int actualSpeed = car.speed;
+        Assertions.assertEquals(expectedSpeed,actualSpeed);
+
+    }
+    @DisplayName("CAr speed by parameterized constructor")
+    @Test
+    void testCarSpeedParameterized(){
+        Car car = new Car(100);
+        int expectedSpeed = 100;
+        int actualSpeed = car.speed;
+        Assertions.assertEquals(expectedSpeed,actualSpeed);
+
+    }
+}
