b4470abb0ebf45fcc0370a815dcb311a98a7f706
prashanth.gns <prashanth.gns@collabera.com>
2022-01-05 13:09:28 +0530

    java recap:exception

---
+++ java-other-concepts/src/main/java/exception/BadNumberException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class BadNumberException extends RuntimeException {
+    public BadNumberException() {
+    }
+
+    public BadNumberException(String message) {
+        super(message);
+    }
+}
