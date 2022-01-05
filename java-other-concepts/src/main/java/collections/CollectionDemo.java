464cb9beeeed723430283e1eb9c7d000091af026
prashanth.gns <prashanth.gns@collabera.com>
2022-01-05 16:06:36 +0530

    java collection

--- java-other-concepts/src/main/java/collections/CollectionDemo.java
+++ java-other-concepts/src/main/java/collections/CollectionDemo.java
@@ -11,7 +11,11 @@ public class CollectionDemo {
         names.add("xyz");
         names.add(null);
         names.add("xyz");
-
+       //     names.clear(); it will delete all elements in list
+        names.add(4, "hji");
+        names.contains("abc");
+        names.remove(2);
+        names.remove("xyz");
         Consumer<String> cls = new Consumer<String>() { // anonymous inner class -> traditional approach
             @Override
             public void accept(String s) {
@@ -52,11 +56,33 @@ public class CollectionDemo {
 
         return queue;
     }
+    public void mapDemo() {
+
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "abc");
+        map.put(2, "pqr");
+        map.put(3, "lmn");
+        map.put(4, "xyz");
+
+        Set<Integer> keys =  map.keySet();
+        keys.forEach( key -> System.out.println("Key - "+key) );
+
+        Collection<String> values = map.values();
+        values.forEach( val -> System.out.println("Value - "+val) );
+
+        map.remove(1, "abc");
+        System.out.println("2 available -> "+map.containsKey(2));;
+        System.out.println("lmn available -> "+map.containsValue("lmn"));;
+        System.out.println("Element At 3 -> "+ map.getOrDefault(3, "none"));
+
+        map.forEach( (k, v) -> System.out.println("Key - "+k +" Value - "+v) );
+    }
+
 
     public static void main(String[] args) {
         CollectionDemo cdm = new CollectionDemo();
-//        cdm.listDemo();
-//        cdm.setDemo();
+      cdm.listDemo();
+        cdm.setDemo();
         cdm.queueDemo();
     }
 }
