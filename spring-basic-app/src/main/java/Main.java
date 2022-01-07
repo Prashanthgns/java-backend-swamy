08b18b850310d1cd36c5ad651edd86f924f284f5
prashanth.gns <prashanth.gns@collabera.com>
2022-01-07 17:58:48 +0530

    spring : dependency injection constructor and setter injection

--- spring-basic-app/src/main/java/Main.java
+++ spring-basic-app/src/main/java/Main.java
@@ -2,20 +2,23 @@ import car.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
 
-            ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-            Car car1 = (Car) context.getBean("carakfjhlskfhslhlshg");
-            car1.setSpeed(100);
+        Car car1 = (Car) context.getBean("car");
+        car1.setSpeed(100);
 
-            Class<Car> cls = Car.class; // this is not the bytecode class file
-            Car car2 = context.getBean(cls);
-            System.out.println("Car Speed is "+ car2.getSpeed());
-
-        }
+        Class<Car> cls = Car.class; // this is not the bytecode class file
+        Car car2 = context.getBean(cls);
+        System.out.println("Car Speed is " + car2.getSpeed());
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
     }
+
+}
 
