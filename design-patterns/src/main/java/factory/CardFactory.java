17a1077812265326fd3b9227c2367407ce305988
prashanth.gns <prashanth.gns@collabera.com>
2022-01-07 15:11:59 +0530

    design factory : abstract

--- design-patterns/src/main/java/factory/CardFactory.java
+++ design-patterns/src/main/java/factory/CardFactory.java
@@ -1,10 +1,20 @@
 package factory;
 
-public abstract class CardFactory {
-    public static CardProvider getCard(String bank) {
-        if(bank.equals("pqr")) return new PqrCard();
-        if(bank.equals("abc")) return new AbcCard();
-        if(bank.equals("xyz")) return new XyzCard();
-        throw new InvalidBankException("ATM Card Facility is not provided by "+bank);
+public /*abstract*/ class CardFactory extends Factory {
+    private CardFactory() {
     }
+
+    public static CardFactory newInstance() {
+        return new CardFactory();
+    }
+
+    public static CardProvider getCard(String bank) {
+        if (bank.equals("pqr")) return new PqrCard();
+        if (bank.equals("abc")) return new AbcCard();
+        if (bank.equals("xyz")) return new XyzCard();
+        throw new InvalidBankException("ATM Card Facility is not provided by " + bank);
+    }
+
+
 }
+
